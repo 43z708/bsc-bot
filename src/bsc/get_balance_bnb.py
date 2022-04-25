09c84ddd19ea9e73f9c1c3f0249ab16a6e00b463
Naoya Shimizu <52235419+43z708@users.noreply.github.com>
2022-04-26 00:39:34 +0900

    Update get_balance_bnb.py

--- src/bsc/get_balance_bnb.py
+++ src/bsc/get_balance_bnb.py
@@ -4,7 +4,7 @@ bsc_rpc = "https://bsc-dataseed.binance.org/"
 
 web3 = Web3(Web3.HTTPProvider(bsc_rpc))
 
-my_wallet_address = '0x298523fAA843CA40e83b7934170ca874a4FA1b04'
+my_wallet_address = ''
 
 balance = web3.eth.getBalance(my_wallet_address)
 
